4e317dfec146848bfc8999bc84a2cd8e7c724f74
Duukkis <duukkis@hotmail.com>
2019-03-03 14:30:27 +0200

    timer functionality

--- src/main.lua
+++ src/main.lua
@@ -1,13 +1,19 @@
 
 -- in lua everything is global
 num = 1
+
+debug = true
+
 screen_width = 640
 screen_height = 480
 -- plumber 17 28
 plumber_x = 30
 plumber_y = 30
-debug = true
 
+---
+max_time = 10
+timer_bar_height = 300
+timer = love.timer.getTime()
 
 function love.load()
     plumber = love.graphics.newImage("images/plumber.png")
@@ -21,9 +27,24 @@ function love.draw()
       love.graphics.print('x,y ' .. plumber_x .. 'x' .. plumber_y, 20, 20)
     end
     love.graphics.draw(plumber, plumber_x, plumber_y)
+    barFiller()
 end
 
 
+-- draw a bar on the side to measure time
+function barFiller()
+  local now = love.timer.getTime()
+  local diff = now - timer
+  
+  if diff > max_time then
+    diff = 0
+    timer = love.timer.getTime()
+  end
+  
+  filled = timer_bar_height * (diff / max_time)
+  love.graphics.rectangle('line', 660, 30, 30, timer_bar_height, 3)
+  love.graphics.rectangle('fill', 660, 30, 30, filled, 3)
+end
 
 function love.update(dt)
    if love.keyboard.isDown("a") or love.keyboard.isDown("left") then
