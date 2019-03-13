6af7b17acd8c9dc1217a8bc4a0d8e1f3ec44d10c
Risto Kuusterä <risto.kuustera@gofore.com>
2019-03-13 19:11:03 +0100

    Something where to bease the actual coding thingie

--- codetris/main.lua
+++ codetris/main.lua
@@ -5,22 +5,33 @@ local BLUE = "blue"
 local RED = "red"
 local YELLOW = "yellow"
 
+local CORRECT = "correct"
+local WRONG = "wrong"
+
 local specs = {}
+local codes = {}
+
+local specImages = {}
+local codeImages = {}
 
 function love.load()
     math.randomseed(os.time())
     love.window.setMode(screen_width, screen_height)
 
-    specImages = {}
     specImages[BLUE] = love.graphics.newImage("images/bleu.png")
     specImages[RED] = love.graphics.newImage("images/rod.png")
     specImages[YELLOW] = love.graphics.newImage("images/yello.png")
+
+    codeImages[CORRECT] = love.graphics.newImage("images/bene.png")
+    codeImages[WRONG] = love.graphics.newImage("images/male.png")
 
     table.insert(specs, createSpec())
 end
 
 function love.draw()
+    love.graphics.print('z = blue, x = red, c = yellow', 200, 40)
     drawSpecs(specs, specImages)
+    drawCodes(codes, codeImages)
 end
 
 function love.update(dt)
@@ -29,8 +40,18 @@ end
 
 function drawSpecs(specs, specImages)
     for index, spec in ipairs(specs) do
-        love.graphics.draw(specImages[spec.specImage], spec.xPos, spec.yPos)
+        drawImage(specImages[spec.image], spec.xPos, spec.yPos)
     end
+end
+
+function drawCodes(codes, codeImages)
+    for index, code in ipairs(codes) do
+        drawImage(codeImages[code.image], code.xPos, code.yPos)
+    end
+end
+
+function drawImage(image, x, y)
+    love.graphics.draw(image, x, y)
 end
 
 function updateSpecs(specs, dt)
@@ -42,7 +63,7 @@ function createSpec()
     spec.xPos = 10
     spec.yPos = -10
     spec.speed = 100
-    spec.specImage = getColorFromNumber(math.random(3))
+    spec.image = getColorFromNumber(math.random(3))
     return spec
 end
 
