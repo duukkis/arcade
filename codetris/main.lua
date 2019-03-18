4a4df9e99119448608c0743f66220ec99bbcf311
Risto Kuusterä <risto.kuustera@gofore.com>
2019-03-18 22:45:22 +0100

    Added second player

--- codetris/main.lua
+++ codetris/main.lua
@@ -1,15 +1,20 @@
-local screen_width = 640
-local screen_height = 480
+local screenWidth = 640
+local screenHeight = 480
 
 local BLUE = "blue"
 local RED = "red"
 local YELLOW = "yellow"
 
+local TO_RIGHT = 1
+local TO_LEFT = -1
+
 local CORRECT = "correct"
 local WRONG = "wrong"
 
-local specs = {}
-local codes = {}
+local player1Specs = {}
+local player2Specs = {}
+local player1Codes = {}
+local player2Codes = {}
 
 local specImages = {}
 local codeImages = {}
@@ -18,7 +23,8 @@ local spawnTimer = 0
 
 function love.load()
     math.randomseed(os.time())
-    love.window.setMode(screen_width, screen_height)
+    love.window.setMode(screenWidth, screenHeight)
+    love.window.setTitle("ⓒⓞⓓⓔⓣⓡⓘⓢ")
 
     specImages[BLUE] = love.graphics.newImage("images/bleu.png")
     specImages[RED] = love.graphics.newImage("images/rod.png")
@@ -31,73 +37,102 @@ function love.load()
 end
 
 function love.draw()
+
+    love.graphics.print('player 1', 200, 20)
     love.graphics.print('z = blue, x = red, c = yellow', 200, 40)
-    love.graphics.print('space = reject errornous', 200, 60)
+    love.graphics.print('a = reject errornous', 200, 60)
 
-    drawSpecs(specs, specImages)
-    drawCodes(codes, codeImages)
+    love.graphics.print('player 2', 200, 100)
+    love.graphics.print('b = blue, n = red, m = yellow', 200, 120)
+    love.graphics.print('k = reject errornous', 200, 140)
 
-    drawImage(guy, 0, screen_height - 30)
+    drawThings(combineTables(player1Specs, player2Specs), specImages)
+    drawThings(combineTables(player1Codes, player2Codes), codeImages)
+
+    drawImage(guy, 10, screenHeight - 30)
+    drawImage(guy, screenWidth - 10, screenHeight - 30, 0, -1)
 end
 
 function love.update(dt)
-    for index, spec in ipairs(specs) do
-        updateLocation(spec, dt)
-        if isOutOfBounds(spec) then
-            table.remove(specs, index)
-            table.insert(codes, createCode(false))
-        end
-    end
+    moveSpecs(player1Specs, player2Codes, dt, TO_RIGHT)
+    moveSpecs(player2Specs, player1Codes, dt, TO_LEFT)
 
-    for index, code in ipairs(codes) do
-        updateLocation(code, dt)
-        if isOutOfBounds(code) then
-            table.remove(codes, index)
-        end
-
-    end
+    moveCodes(player1Codes, dt)
+    moveCodes(player2Codes, dt)
 
     if spawnTimer > 0 then
         spawnTimer = spawnTimer - dt
     else
-        table.insert(specs, createSpec())
+        table.insert(player1Specs, createSpec(10))
+        table.insert(player2Specs, createSpec(screenWidth - 40))
         spawnTimer = 1
     end
 end
 
 function love.keypressed(key)
     if key == "z" then
-        handleCoding(BLUE, specs)
+        handleCoding(BLUE, player1Specs, player2Codes, TO_RIGHT)
     end
 
     if key == "x" then
-        handleCoding(RED, specs)
+        handleCoding(RED, player1Specs, player2Codes, TO_RIGHT)
     end
 
     if key == "c" then
-        handleCoding(YELLOW, specs)
+        handleCoding(YELLOW, player1Specs, player2Codes, TO_RIGHT)
     end
 
-    if key == "space" then
-        handleReject(codes)
+    if key == "a" then
+        handleReject(player1Codes)
     end
 
+    if key == "b" then
+        handleCoding(BLUE, player2Specs, player1Codes, TO_LEFT)
+    end
+
+    if key == "n" then
+        handleCoding(RED, player2Specs, player1Codes, TO_LEFT)
+    end
+
+    if key == "m" then
+        handleCoding(YELLOW, player2Specs, player1Codes, TO_LEFT)
+    end
+
+    if key == "k" then
+        handleReject(player2Codes)
+    end
 end
 
-function drawSpecs(specs, specImages)
+function drawThings(things, thingImages)
+    for index, thing in ipairs(things) do
+        drawImage(thingImages[thing.image], thing.xPos, thing.yPos)
+    end
+end
+
+function drawImage(image, x, y, r, sx, sy)
+    r = r or 0
+    sx = sx or 1
+    sy = sy or 1
+    love.graphics.draw(image, x, y, r, sx, sy)
+end
+
+function moveSpecs(specs, resultCodes, dt, direction)
     for index, spec in ipairs(specs) do
-        drawImage(specImages[spec.image], spec.xPos, spec.yPos)
+        updateLocation(spec, dt)
+         if isOutOfBounds(spec) then
+            table.remove(specs, index)
+            table.insert(resultCodes, createCode(false, direction))
+        end
     end
 end
 
-function drawCodes(codes, codeImages)
+function moveCodes(codes, dt)
     for index, code in ipairs(codes) do
-        drawImage(codeImages[code.image], code.xPos, code.yPos)
+        updateLocation(code, dt)
+        if isOutOfBounds(code) then
+            table.remove(codes, index)
+        end
     end
-end
-
-function drawImage(image, x, y)
-    love.graphics.draw(image, x, y)
 end
 
 function updateLocation(thing, dt)
@@ -105,9 +140,9 @@ function updateLocation(thing, dt)
     thing.xPos = thing.xPos + dt * thing.speedX
 end
 
-function createSpec()
+function createSpec(xPos)
     return {
-        xPos = 10,
+        xPos = xPos,
         yPos = -10,
         speedY = 100,
         speedX = 0,
@@ -127,18 +162,24 @@ function getColorFromNumber(num)
     end
 end
 
-function createCode(isCorrect)
+function createCode(isCorrect, direction)
     local correctness = WRONG
 
     if isCorrect then
         correctness = CORRECT
     end
 
+    if direction == TO_RIGHT then
+        xPos = 10
+    else
+        xPos = screenWidth - 10
+    end
+
     return {
-        xPos = 10,
-        yPos = screen_height - 32 - 10,
+        xPos = xPos,
+        yPos = screenHeight - 32 - 10,
         speedY = 0,
-        speedX = 100,
+        speedX = 100 * direction,
         image = correctness,
         width = 32,
         height = 32
@@ -146,16 +187,17 @@ function createCode(isCorrect)
 end
 
 function isOutOfBounds(thing)
-    return thing.yPos > screen_height - thing.height or
-            thing.xPos > screen_width
+    return thing.yPos > screenHeight - thing.height or
+            thing.xPos < 0 or
+            thing.xPos > screenWidth
 end
 
-function handleCoding(color, specs)
+function handleCoding(color, specs, resultCodes, direction)
     for index, spec in ipairs(specs) do -- aargh!
         if color == spec.image then
-            table.insert(codes, createCode(true))
+            table.insert(resultCodes, createCode(true, direction))
         else
-            table.insert(codes, createCode(false))
+            table.insert(resultCodes, createCode(false, direction))
         end
         table.remove(specs, index)
         break
@@ -169,4 +211,17 @@ function handleReject(codes)
         end
         break
     end
+end
+
+function combineTables(t1, t2)
+    combined = {}
+    for k, value in ipairs(t1) do
+        table.insert(combined, value)
+    end
+
+    for k, value in ipairs(t2) do
+        table.insert(combined, value)
+    end
+
+    return combined
 end
