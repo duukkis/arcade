6f1893ebbb79595b71d802e5582e202862efa50c
Mikael Harju <mikael.harju@gofore.com>
2019-05-30 14:41:14 +0300

    Add keijoGame

---
+++ keijoGame/main.lua
@@ -0,0 +1,34 @@
+local SCREEN_WIDTH = 640
+local SCREEN_HEIGHT = 480
+local PIPE_X_OFFSET = 20
+local PIPE_Y_OFFSET = 430
+
+local BLUE = "blue"
+local RED = "red"
+local GREEN = "green"
+local PIPE = "pipe"
+local COLOR_COUNT = 3
+local lanes = {}
+local images = {}
+
+function love.load()
+  love.window.setMode(SCREEN_WIDTH, SCREEN_HEIGHT)
+  love.window.setTitle("K E I J O G A M E")
+  images[PIPE] = love.graphics.newImage("images/rosso.png")
+end
+
+function love.draw()
+  drawPipes()
+end
+
+function love.update(dt)
+
+end
+
+function drawPipes()
+  local localOffset = 140
+  for i = 0,4 do
+    love.graphics.draw(images[PIPE], PIPE_X_OFFSET + (i * localOffset), PIPE_Y_OFFSET)
+  end
+  
+end
