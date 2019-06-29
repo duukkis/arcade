e2330adac1fe40735439772490ae815f8d554a64
Pentti Laitinen <pentti.laitinen@pm.me>
2019-06-29 23:50:20 +0300

    Added basic remote functionality
    - Current implementation uses one button for power toggle and other for
    changing the input of the TV.

--- remote/src/main.cpp
+++ remote/src/main.cpp
@@ -1,20 +1,34 @@
 #include <Arduino.h>
 #include <IRremote.h>
 
-#define CHANNEL_UP 0x20DF00FF
-#define INPUT_CHANGE 0x20DFD02F
-#define SIGNAL_BITS 28
-
-#define SEND_DELAY 1000
-
 IRsend irsend;
 
-void setup() {}
+unsigned int powerToggle = 0x20;
+unsigned int intputNext = 0x82A;
+unsigned int volumeUp = 0x24;
+unsigned int volumeDown = 0x825;
+
+unsigned int inputs[2][2] = {
+    {2, 3},
+    {powerToggle, intputNext}
+};
+
+void setup()
+{
+  for (size_t button = 0; button < (sizeof inputs[0] / sizeof(int)); button++)
+  {
+    pinMode(inputs[0][button], INPUT_PULLUP);
+  }
+}
 
 void loop()
 {
-  // TODO send sequence if button pressed
-  irsend.sendLG(CHANNEL_UP, SIGNAL_BITS);
-  delay(SEND_DELAY);
-  irsend.sendLG(INPUT_CHANGE, SIGNAL_BITS)
+  for (size_t button = 0; button < (sizeof inputs[0] / sizeof(int)); button++)
+  {
+    if (digitalRead(inputs[0][button]) == LOW)
+    {
+      irsend.sendRC5(inputs[1][button], 12);
+      delay(500);
+    }
+  }
 }
