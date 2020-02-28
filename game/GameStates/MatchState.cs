a151ddf08685271b2407f9d4ae38360177b86fb4
Jaakko Alanen <jaakko.alanen@gofore.com>
2020-02-28 16:45:00 +0200

    Add player1 and player2 and draw them to MatchState

--- game/GameStates/MatchState.cs
+++ game/GameStates/MatchState.cs
@@ -6,19 +6,27 @@ namespace Arcade.GameStates
 {
   public class MatchState : GameState
   {
+    public Player PlayerOne;
+    public Player PlayerTwo;
+    private GraphicsDevice graphicsDevice;
 
     public MatchState(GraphicsDevice graphicsDevice) : base(graphicsDevice)
     {
-
+      this.graphicsDevice = graphicsDevice;
     }
+
     public override void Initialize()
     {
-
+      PlayerOne = new Player();
+      PlayerTwo = new Player();
     }
 
     public override void LoadContent(ContentManager content)
     {
-
+      Vector2 playerOnePosition = new Vector2(graphicsDevice.Viewport.TitleSafeArea.X, graphicsDevice.Viewport.TitleSafeArea.Y + graphicsDevice.Viewport.TitleSafeArea.Height / 2);
+      Vector2 playerTwoPosition = new Vector2(graphicsDevice.Viewport.TitleSafeArea.X + 200, graphicsDevice.Viewport.TitleSafeArea.Y + graphicsDevice.Viewport.TitleSafeArea.Height / 2);
+      PlayerOne.Initialize(content.Load<Texture2D>("player1"), playerOnePosition);
+      PlayerTwo.Initialize(content.Load<Texture2D>("player2"), playerTwoPosition);
     }
 
     public override void UnloadContent()
@@ -33,8 +41,10 @@ namespace Arcade.GameStates
 
     public override void Draw(SpriteBatch spriteBatch)
     {
-      _graphicsDevice.Clear(Color.Black);
+      graphicsDevice.Clear(Color.Black);
       spriteBatch.Begin();
+      PlayerOne.Draw(spriteBatch);
+      PlayerTwo.Draw(spriteBatch);
       spriteBatch.End();
     }
   }
---
+++ game/Player/Player.cs
@@ -0,0 +1,35 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Arcade
+{
+  public class Player
+  {
+    public Texture2D PlayerTexture;
+    public Vector2 Position;
+    public int Width
+    {
+      get { return PlayerTexture.Width; }
+    }
+    public int Height
+    {
+      get { return PlayerTexture.Height; }
+    }
+
+    public void Initialize(Texture2D texture, Vector2 position)
+    {
+      PlayerTexture = texture;
+      Position = position;
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+      spriteBatch.Draw(PlayerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+    }
+  }
+}
