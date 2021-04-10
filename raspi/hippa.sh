a7c39e7976a6634a43f99be0dc9e21a891c2f516
duukkis <duukkis@hotmail.com>
2021-04-10 14:13:39 +0300

    delete before

--- raspi/hippa.sh
+++ raspi/hippa.sh
@@ -5,6 +5,10 @@ sed 's/\// /g') | cut -f1) ]; then
   echo up to date
 else
   echo not up to date
+  rm ./LinuxX11.zip
+  rm -fr ./data_HagatonHippa
+  rm -fr ./hagatonhippa-linux.pck
+
   git pull
   curl -s https://api.github.com/repos/duukkis/arcade/releases/latest \
   | grep "LinuxX11.zip" \
@@ -13,7 +17,6 @@ else
   | wget -qi -
 
   unzip -o LinuxX11.zip
-  rm ./LinuxX11.zip
   ps -ef | grep 'godot_3.2.3_rpi4_export-template' | grep -v grep | awk '{print $2}' | xargs kill
   cd /home/pi/godot
   ./godot_3.2.3_rpi4_export-template.bin --main-pack "../arcade/raspi/hagatonhippa-linux.pck"
