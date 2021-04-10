01139f9ec902afcb7a8a3bbc73cd3b098a58d8c2
Pentti Laitinen <pentti.laitinen@gofore.com>
2021-04-10 10:13:48 +0300

    Fix formating
    - Fix player formating.
    - Add linux output file to git ignore.

--- hagatonhippa/player/Player.cs
+++ hagatonhippa/player/Player.cs
@@ -7,7 +7,7 @@ public class Player : KinematicBody2D
     [Export] public int WALK_MAX_SPEED = 200;
     [Export] public int STOP_FORCE = 2000;
     [Export] public int JUMP_SPEED = 200;
-    public Vector2 velocity = new Vector2();
+    public Vector2 Velocity;
     private int gravity;
 
     public override void _Ready()
@@ -25,22 +25,22 @@ public class Player : KinematicBody2D
         float walk = WALK_FORCE * (Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"));
         if (Mathf.Abs(walk) < WALK_FORCE * 0.2)
         {
-            velocity.x = Mathf.MoveToward(velocity.x, 0, STOP_FORCE * delta);
+            Velocity.x = Mathf.MoveToward(Velocity.x, 0, STOP_FORCE * delta);
         }
         else
         {
-            velocity.x += walk * delta;
+            Velocity.x += walk * delta;
         }
 
-        velocity.x = Mathf.Clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED);
+        Velocity.x = Mathf.Clamp(Velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED);
 
-        velocity.y += gravity * delta;
+        Velocity.y += gravity * delta;
 
-        velocity = MoveAndSlideWithSnap(velocity, Vector2.Down, Vector2.Up);
+        Velocity = MoveAndSlideWithSnap(Velocity, Vector2.Down, Vector2.Up);
 
         if (IsOnFloor() && Input.IsActionJustPressed("jump"))
         {
-            velocity.y = -JUMP_SPEED;
+            Velocity.y = -JUMP_SPEED;
         }
     }
 }
